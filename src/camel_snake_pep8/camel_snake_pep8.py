666aa6aa9eb0c7c3aa237734744bebac49414769
Allen Barker <Allen.L.Barker@gmail.com>
2017-03-08 19:34:48 -0500

    warn on conversion collisions too

--- src/camel_snake_pep8/camel_snake_pep8.py
+++ src/camel_snake_pep8/camel_snake_pep8.py
@@ -2,108 +2,18 @@
 """
 
 camel-snake-pep8
-===================
+================
 
 A refactoring tool to help convert camel case to snake case and vice versa in a
-Python program, in conformity with the PEP-8 style guide.
+Python program, in conformity with the PEP-8 style guide.  It uses/abuses
+Python-Rope to do find and perform the changes.
 
-.. warning::
+**Use this software at your own risk.**
 
-   **Use this software at your own risk.**  This program has various features
-   to try to ensure that no errors are introduced, but correctness cannot be
-   guaranteed.  Always make a backup copy of any project before running this
-   program on it.  The program has been used a few times with good results, but
-   does not currently have formal tests.
+Currently only runs on Python 2, but can refactor Python 2 and Python 3.
 
-Installing and using
---------------------
-
-To install the program just clone or download the git repository then execute
-the main file, ``camel_snake_pep8.py``.  The program is currently a single
-module.
-
-Usage::
-
-   camel_snake_pep8.py <projectDir> <fileToModify> [<fileToModify> ...]
-
-For example, to change all the files in a project go to the main source
-directory (package root if a package) of the project to be refactored and
-type::
-
-    camel_snake_pep8.py . *.py
-
-Be sure to include any subpackage modules, too, if there are subpackages.
-
-How it works
-------------
-
-This program uses/abuses Python-Rope to detect variables to possibly change.
-It then queries the user and makes any user-approved changes.  This program can
-be used to modify Python 2 or Python 3 code, but it must be run with Python 2
-(Python-Rope only supports Python2 as of Mar 2017; a port is said to be in
-progress.)
-
-The program cycles through each potential change in for each file specified,
-querying as to whether to keep or reject the proposed change.  The files and
-names are all re-processed each time, since offsets can change with each
-modification.  The running time is nevertheless not bad for interactive use.
-
-Warnings and theory
--------------------
-
-The program tries to make the refactoring as safe as possible, since bugs
-introduced by bad renaming can be difficult to find.  The real danger with
-renaming operations is name collisions.  Name collisions can occur because Rope
-will happily rename a variable to a name that is already in use in the same
-scope.  For example, a function parameter could be renamed to collide with a
-preexisting local variable inside the function.  Here is an example:
-
-code-block:: python
-
-   def f(camelArg):
-       camelArg = 555
-       camel_arg = 444
-       return camelArg
-
-If the change of the parameter ``camelArg`` to ``camel_arg`` is accepted
-(despite the warning) the new function will return 444, not 555.
-
-Warnings are issued for possible situations which may lead to a collision (or
-may not, since scoping is not taken into account).  The default query reply,
-such as when the user just hits "enter" each time, is set to accept the change
-when no warning is given and reject the change when a warning is given.  Many
-of the changes with warnings will actually be safe, but before accepting them
-users should carefully inspect the diffs for the change (and possibly the files
-themselves) to be sure.  As an alternative, a slightly different snake case
-name can be tried by hitting ``c`` in respose to the query.
-
-It is better to make all the changes in one run of the program, since the
-program collects all the existing names (per module) before starting in order
-to warn about possible collisions.
-
-.. note::
-
-    Rough "proof" of reasonable safety for changes without warnings and
-    assuming that Python-Rope does the name replacements correctly.
-
-    1. Camel case strings and snake case are disjoint sets of names.
-
-    2. If no occurrences of the new snake case string exist in any file where
-    changes are made then all the corresponding camel case strings should be
-    converted to that value.  (If the string does exist in one of those files a
-    warning will be given.)  No name collisions can occur because the new name
-    did not exist in any of those files in the first place.  Any variables
-    which end up with the same name had the same name in the first place.
-
-    Of course since Python is dynamic and has introspection there will always
-    be cases where the rename substitutions fail (such as modifying the globals
-    dict).
-
-    Other possible problems can arise from cases where Rope cannot resolve a
-    proposed change and so that change is skipped even though it is
-    semantically necessary.  The program does an analysis after all the changes
-    are made which looks for possible problems in that regard, and warnings are
-    issued if any are found.
+..  Copyright (c) 2017 by Allen Barker.
+    License: MIT, see LICENSE for more details.
 
 """
 
@@ -117,6 +27,7 @@ import itertools
 from collections import defaultdict
 
 from rope.base.project import Project
+from rope.base.libutils import get_string_scope, get_string_module # Experiment with.
 from rope.refactor.rename import Rename
 from rope.base import worder
 from colorama import Fore, Back, Style
@@ -157,7 +68,7 @@ fname_list = sys.argv[2:]
 # Dicts and sets for saving names from files and related functions.
 #
 
-original_change_sets_dict = {} # Original names in files, keyed by realpath to the files.
+original_names_sets_dict = {} # Original names in files, keyed by realpath to the files.
 final_names_sets_dict = {} # The final names in files, after all changes.
 
 modified_modules_set = set() # The realpaths of modified modules.
@@ -175,9 +86,9 @@ user_rejected_changes_sets_dict = defaultdict(set) # Changes rejected by the use
 rope_rejected_changes_sets_dict = defaultdict(set) # Changes rejected by rope.
 
 def save_changes(realpath_list, change, user=True, accepted=True):
-    """Save one rejected change keyed by the corresponding module pathname.  Offset
-    information is removed from the middle of any 3-tuple changes since it does
-    not remain valid."""
+    """Save one change or rejected change, keyed by the corresponding module pathnames
+    in `realpath_list`.  Offset information is removed from the middle of any 3-tuple
+    changes since it does not remain valid."""
     if len(change) > 2:
         change = (change[1], change[3])
     for path in realpath_list:
@@ -189,7 +100,7 @@ def save_changes(realpath_list, change, user=True, accepted=True):
             rope_rejected_changes_sets_dict[path].add(change)
 
 def compare_changes_with_final_names(module_realpath_list, changes_dict, accepted=True):
-    """This routine does the real workn for `analyze_names_in_final_state`, looping
+    """This routine does the real work for `analyze_names_in_final_state`, looping
     over the designated changes and modules."""
     if accepted:
         accept_or_reject_word = "Accepted"
@@ -233,10 +144,12 @@ def analyze_names_in_final_state(module_realpath_list):
     another, but not on purpose.  Vice versa for accepted changes."""
 
     print_banner("Doing post-processing name analysis on all modules.", big=True)
+
     # Get the final names from all the modules.
     for module_realpath in module_realpath_list:
         save_set_of_all_names_in_module(module_realpath, save_dict=final_names_sets_dict)
 
+    # Issue warnings.
     print_warning("Any warnings below are only for potential problems.  Most will probably"
                   "\nnot be problems.  No scoping information is taken into account in"
                   " this\nanalysis.\n")
@@ -386,20 +299,16 @@ def unique_everseen(iterable, key=None):
                 yield element
 
 #
-# Parsing function parameter strings to get parameters without default values.
+# Parsing function parameter strings (to find the parameters without default values).
 #
 
 def process_param(param, offset):
     """Process a single parameter produced by `get_function_parameter_names`."""
-    #print("   arg being processed:", param, offset)
-
     # Ignore args with default values, since Rope considers them assignments.
     if "=" in param:
-        #print("   returning arg:", [])
         return []
 
     # Strip off any type annotation.
-    # TODO: below, splitting on : you need to take the left side not right!
     first_colon_index = param.find(":")
     if first_colon_index >= 0: # Variables are first in MyPy, reversed from C.
         param = param[:first_colon_index]
@@ -410,26 +319,29 @@ def process_param(param, offset):
     param = param.strip()
     if not param:
         return []
-    #print("   returning arg:", param, offset)
     return [param, offset]
 
-def get_function_param_names(initial_fun_string, initial_offset):
-    """Parse a function string to get the parameter names which are
-    not assigned default values (since those are taken care of in the
-    variable-assignment group)."""
+def get_function_param_names(initial_fun_string, initial_offset, fun_name_string):
+    """Parse a function string returned by Rope's
+    `get_function_and_args_in_header` to get the parameter names which are not
+    assigned default values (since those are taken care of in the
+    variable-assignment group).  The function name `fun_name_string` is passed
+    only as an error check to make sure the name found in the initial offset
+    search matches the name in the string returned by the Rope function."""
     fun_string = initial_fun_string
     offset = initial_offset
 
     if not fun_string:
         return []
-    #print("\ninitial fun string is:", fun_string)
+
     # Do some initial preprocessing.
     index = fun_string.find("(") + 1
+    fun_name = fun_string[:index-1]
+    assert fun_name == fun_name_string # Error check.
     fun_string = fun_string[index:].split("->")[0] # Remove name and return type.
     fun_string = fun_string.rstrip()
     offset += index
     index = 0 # Keep a local index relative to first char of first arg.
-    #print("fun string is:", fun_string)
 
     # Make into a list of characters.
     close_paren_index = fun_string.rfind(")") # Note we need rfind here.
@@ -453,17 +365,14 @@ def get_function_param_names(initial_fun_string, initial_offset):
         else:
             simplified_fun_list.append(c)
     fun_string = "".join(simplified_fun_list)
-    #print("fun string after quashing is:", fun_string)
 
     # Separate the arguments and call process_params on them.
     final_name_list = []
     while True:
         comma_index = fun_string.find(",", index)
-        #print("index is", index, "comma index is", comma_index)
         if comma_index < 0:
             break
         arg_string = fun_string[index:comma_index]
-        #print("passing in arg", arg_string)
         if arg_string:
             name_and_offset = process_param(arg_string, offset + index)
             if name_and_offset:
@@ -473,17 +382,38 @@ def get_function_param_names(initial_fun_string, initial_offset):
     for n in final_name_list:
         for i in range(len(n[0])):
             assert n[0][i] == initial_fun_string[n[1]-initial_offset + i]
-    #print("returning args:", final_name_list)
     return final_name_list
 
 #
 # Functions that do the real work.
 #
 
+def experiment_with_scoping_classes(project, source_file_name):
+    """Just for experimenting with `PyObject` and `Scope` objects."""
+    def dir_no_magic(obj):
+        return [s for s in dir(obj) if s[:2] != "__"]
+
+    source_string = get_source_string(source_file_name)
+
+    # Get a `Worder` for the code.
+    w = worder.Worder(source_string)
+    print("\nGot a Worder.  The dir is", dir_no_magic(w))
+
+    # Get a PyObject for the code.
+    py_object = get_string_module(project, source_string, resource=None, force_errors=False)
+    print("\nGot a PyObject.  The dir is", dir_no_magic(py_object))
+
+    # Get a Scope object for the code.
+    scope_object = get_string_scope(project, source_string, resource=None)
+    print("\nGot a Scope object.  The dir is", dir_no_magic(scope_object))
+    print("The names in the Scope are:\n", scope_object.get_names())
+
+
 def rope_iterate_worder(source_file_name, fun_name_defs=False, fun_arguments=False,
                         fun_keywords=False, assigned_vars=False, class_names=False,
                         unfiltered=False):
-    """Get all the names of a given type and their offsets.
+    """Get all the names of a given type and their offsets.  The `project` argument
+    is not currently used.
 
     Due to how rope works these are split up in an unusual way.  The function arguments
     without default values are parsed out of the string representing the function
@@ -535,22 +465,26 @@ def rope_iterate_worder(source_file_name, fun_name_defs=False, fun_arguments=Fal
             upcoming = None
 
             try:
-                # TODO? NOTE: Adding -10 below was needed to make the CURRENT
-                # fun name detected match the function and args returned below!
-                # Otherwise, you always got a fun name, but got the string for
-                # the one that is ahead in text...  This also makes the offsets
-                # match better...
+                # TODO? NOTE: Adding -4 to offset below was needed to make the
+                # CURRENT function name being detected in this branch of the
+                # `if` match the function and args that are returned by the
+                # call below!  Otherwise, you always got a function name, but
+                # the function below returned the string for the one that is
+                # ahead in text...
                 #
-                # NOTE that -4 is the minimum abs to make them match, and it makes
-                # the offsets exactly match the ones found below in
-                # "unidentified" section... this works, but I do not know why.
+                # The value -4 is the minimum (in abs) to make them match, and
+                # it makes the offsets of the parameters without default
+                # arguments exactly match the ones found below in the
+                # "unidentified" section... This works, but I do not know why.
                 fun_and_args = w.get_function_and_args_in_header(offset-4)
             except (ValueError, IndexError):
                 fun_and_args = None
             #print("Fun and args string from rope is:", fun_and_args)
             if fun_arguments:
-                param_and_offset_list = get_function_param_names(fun_and_args, offset)
-                param_list_and_new = [n + [camel_to_snake(n[0])] for n in param_and_offset_list]
+                param_and_offset_list = get_function_param_names(
+                                                           fun_and_args, offset, word)
+                param_list_and_new = [n + [camel_to_snake(n[0])]
+                                               for n in param_and_offset_list]
                 #print("param_list_and_new", param_list_and_new)
                 possible_changes += param_list_and_new
 
@@ -605,7 +539,7 @@ def get_renaming_changes(project, module, offset, new_name, name, source_file_na
                                        new_name, docs=True, unsure=None)
         return changes
     except rope.base.exceptions.RefactoringError:
-        print("Error in performing a rename from '{0}' to '{1}' in file"
+        print("Error in calculating a rename from '{0}' to '{1}' in file"
               "\n   {2}".format(name, new_name, source_file_name))
     return None
 
@@ -639,20 +573,30 @@ def rope_rename_refactor(project, source_file_name, possible_changes):
 
             # Calculate changed resources and possible warnings.
             warning = False # Could just use warning_modules list in place of boolean...
-            warning_modules = []
+            existing_name_modules = []
+            conversion_collisions = []
             modules_to_change_realpaths = []
             modules_to_change_names = []
             for c in changed_resources:
                 #print("   Path of resource changed:", c.path)
                 #print("   Name of resource changed:", c.name)
                 #print("   Name of resource changed:", c.real_path)
+
+                # Warnings for new name originally in module that would be changed.
                 modules_to_change_realpaths.append(c.real_path)
                 modules_to_change_names.append(c.name)
-                save_set_of_all_names_in_module(c.real_path, save_dict=original_change_sets_dict)
-                if new_name in original_change_sets_dict[c.real_path]:
+                save_set_of_all_names_in_module(
+                        c.real_path, save_dict=original_names_sets_dict)
+                if new_name in original_names_sets_dict[c.real_path]:
                     warning = True
-                    warning_modules.append(c.name)
+                    existing_name_modules.append(c.name)
                 modified_modules_set.add(c.real_path)
+
+                # Warning for name collision with previous change.
+                for accepted_name, accepted_new_name in user_accepted_changes_sets_dict[c.real_path]:
+                    if new_name == accepted_new_name and accepted_name != name:
+                        warning = True
+                        conversion_collisions.append([c.name, accepted_name, accepted_new_name])
 
             # Colorize the description and print it out for the user to view.
             color_new_name = color(NEW_NAME_COLOR, new_name)
@@ -667,16 +611,28 @@ def rope_rename_refactor(project, source_file_name, possible_changes):
             print()
 
             # Print any warnings.
-            if warning:
-                print_color(YELLOW_WARNING_COLOR,
-                        "Caution: The new name '{0}' already existed somewhere"
-                        " in the selected\nmodules before this run of the program made"
+            if existing_name_modules:
+                print_warning(
+                        "Warning: The new name '{0}' already existed somewhere in"
+                        " the modules\nto change before this run of the program made"
                         " any changes.  This may or may not\ncause a name collision."
                         " Scoping was not taken into account in the analysis.\n"
                         "\nThe modules it was found in are:"
                         .format(new_name))
-                for m in warning_modules:
-                    print_color(YELLOW_WARNING_COLOR, "   ", m)
+                for m in existing_name_modules:
+                    print_warning("   ", m)
+                print()
+            if conversion_collisions:
+                print_warning(
+                        "Warning: Already accepted a rename of a different name to the"
+                        " new name\n'{0}' in one of the modules to change.  This"
+                        " may or may not\ncause a name collision.  Scoping was not taken"
+                        " into account in the analysis.\n"
+                        "\nThe modules and previously-accepted changes are:"
+                        .format(new_name))
+                for m in conversion_collisions:
+                    print_warning("   In '{0}' changed '{1}' to '{2}'."
+                                  .format(m[0], m[1], m[2]))
                 print()
 
             # Query the user.
@@ -765,6 +721,7 @@ def main():
     print()
 
     for filename in fname_list:
+        experiment_with_scoping_classes(project, filename)
         print_banner("Python module name: " + filename_to_module_name(filename),
                      char="%", big=True)
 
--- src/camel_snake_pep8/test/test_module_test_on_a_copy.py
+++ src/camel_snake_pep8/test/test_module_test_on_a_copy.py
@@ -1,7 +1,12 @@
 
 from __future__ import print_function, division
 
+#
+# Test collisions due to Rope renaming.
+#
+
 def camelFunction(camelArg1, camelArg2, camelArg3, camelArg4, camelArg5):
+    """Warning on this is correct, gives error."""
     camel_arg4 = 333
     class snake_class(object):
         camelArg5 = camelArg3
@@ -11,4 +16,17 @@ def camelFunction(camelArg1, camelArg2, camelArg3, camelArg4, camelArg5):
     snake_class.camelArg5 = camelArg5
     camelArg4 = 999
     return camel_arg4
+
+a_a = 44
+def fF(aA, a, bB, b, cC, c, dD=3, d=a_a): # Test short names and rename to global.
+    """Warning is not correct on this, does not actually cause error.
+    Resulting code is correct, but `aA` is not converted when warning is heeded."""
+    pass
+
+#
+# Test collision when the two suggested strings collide.
+#
+
+myVar = 5
+myVAR = 44
 
